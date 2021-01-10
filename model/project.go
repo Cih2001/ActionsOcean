bff7397f7da111c3661e8737e67ab2ef62baecf5
Hamidreza Ebtehaj <hamidreza.ebtehaj@gmail.com>
2021-01-10 21:04:25 +0100

    Project model almost done

--- model/project.go
+++ model/project.go
@@ -1,13 +1,57 @@
 package model
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type ProjectModel struct {
 	ID             primitive.ObjectID `bson:"_id, omitempty"`
 	Name           string
 	OwenerID       string
-	OwenerName     string
 	State          string
 	Progress       uint
 	ParticipantsID []string
+}
+
+const (
+	ProjectsCollectionName = "projects" // TODO: can be in a config file if not fixed
+)
+
+func (model *ProjectModel) Find(id string) error {
+	if id == "" {
+		return ErrEmptyID
+	}
+	collection := mongoClient.Database(dbname).Collection(ProjectsCollectionName)
+	objID, _ := primitive.ObjectIDFromHex(id)
+	err := collection.FindOne(context.Background(), bson.M{"_id": objID}).Decode(model)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (model *ProjectModel) Create() (primitive.ObjectID, error) {
+	collection := mongoClient.Database(dbname).Collection(ProjectsCollectionName)
+	model.ID = primitive.NewObjectID()
+	result, err := collection.InsertOne(context.Background(), model)
+	if err != nil {
+		return primitive.ObjectID{}, err
+	}
+	return result.InsertedID.(primitive.ObjectID), nil
+}
+
+func (model *ProjectModel) Delete(id string) error {
+	if id == "" {
+		return ErrEmptyID
+	}
+	collection := mongoClient.Database(dbname).Collection(ProjectsCollectionName)
+	objID, _ := primitive.ObjectIDFromHex(id)
+	_, err := collection.DeleteOne(context.Background(), bson.M{"_id": objID})
+	if err != nil {
+		return err
+	}
+	return nil
 }
---
+++ model/project_test.go
@@ -0,0 +1,66 @@
+package model
+
+import (
+	"errors"
+	"os"
+	"testing"
+)
+
+func connectToLocalDB() error {
+	if mongoClient != nil {
+		return nil
+	}
+	username := os.Getenv("DBUSERNAME")
+	password := os.Getenv("DBPASSWORD")
+	if username == "" || password == "" {
+		return errors.New("we are in build server")
+	}
+
+	var err error
+	mongoClient, err = connectToDB("mongodb://localhost:27017", username, password)
+	return err
+}
+
+func TestProjectFind(t *testing.T) {
+	if err := connectToLocalDB(); err != nil {
+		// we are in build server.
+		return
+	}
+
+	project := ProjectModel{}
+	err := project.Find("1231235")
+	if err == nil {
+		t.Fatal("a document was find with empty id")
+	}
+}
+
+func TestProject(t *testing.T) {
+	if err := connectToLocalDB(); err != nil {
+		// we are in build server.
+		return
+	}
+
+	project := ProjectModel{
+		Name:           "interstellar",
+		OwenerID:       "fake id",
+		State:          "fake state",
+		Progress:       100,
+		ParticipantsID: []string{"fake id1"},
+	}
+	id, err := project.Create()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tmp := ProjectModel{}
+	err = tmp.Find(id.Hex())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = tmp.Delete(tmp.ID.Hex())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+}
